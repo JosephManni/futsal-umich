3464b515f332836225cac702c402123c70734283
gabehutt <gabehutt@umich.edu>
2024-08-14 15:53:44 -0400

    profile redirection

--- components/user.js
+++ components/user.js
@@ -18,7 +18,7 @@ export function ProfileClient() {
 
   return (
     <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
-      <a href="/profile" className="block px-4 py-2 text-lg text-gray-700" role="menuitem" tabIndex="-1">Your Profile</a>
+      <a href="/players/portal" className="block px-4 py-2 text-lg text-gray-700" role="menuitem" tabIndex="-1">Your Profile</a>
       <a href="/api/auth/logout" className="block px-4 py-2 text-lg text-gray-700" role="menuitem" tabIndex="-1">Sign out</a>
     </div>
   );
