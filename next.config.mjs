eef501f424db0b266f8b3f0908250ec77c98978d
Joseph Manni <dev.jmanni@gmail.com>
2024-08-26 13:26:25 -0400

    try2

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['lh3.googleusercontent.com'],
-    },
-    async headers() {
-      return [
-        {
-          source: "/api/(.*)",
-          headers: [
-            {
-              key: "Access-Control-Allow-Origin",
-              value: process.env.NEXT_PUBLIC_ALLOWED_ORIGIN, // Set your origin
-            },
-            {
-              key: "Access-Control-Allow-Methods",
-              value: "GET, POST, PUT, DELETE, OPTIONS",
-            },
-            {
-              key: "Access-Control-Allow-Headers",
-              value: "Content-Type, Authorization",
-            },
-          ],
-        },
-      ];
-    },
-    // Optionally, add any other Next.js config below.
-  };
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+},
+};
 
 export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         domains: ['lh3.googleusercontent.com'],
+//     },
+//     async headers() {
+//       return [
+//         {
+//           source: "/api/(.*)",
+//           headers: [
+//             {
+//               key: "Access-Control-Allow-Origin",
+//               value: process.env.NEXT_PUBLIC_ALLOWED_ORIGIN, // Set your origin
+//             },
+//             {
+//               key: "Access-Control-Allow-Methods",
+//               value: "GET, POST, PUT, DELETE, OPTIONS",
+//             },
+//             {
+//               key: "Access-Control-Allow-Headers",
+//               value: "Content-Type, Authorization",
+//             },
+//           ],
+//         },
+//       ];
+//     },
+//     // Optionally, add any other Next.js config below.
+//   };
+
+// export default nextConfig;
