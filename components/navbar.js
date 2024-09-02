c558c6a27ae10f4f1bae64a0220f1476c4e33058
Joseph Manni <dev.jmanni@gmail.com>
2024-09-02 12:54:25 -0400

    remove tryouts

--- components/navbar.js
+++ components/navbar.js
@@ -35,7 +35,7 @@ export default function Navbar() {
 
     return(
         <nav className="bg-darkblue fixed w-full absolute z-500 top-0">
-            <Announcement />
+            {/* <Announcement /> */}
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
