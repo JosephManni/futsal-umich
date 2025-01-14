73c42d48b95096ae2e16a02750ced5c5a38a1a99
Joseph Manni <dev.jmanni@gmail.com>
2025-01-14 15:16:52 -0500

    live tryouts

--- components/announcement.js
+++ components/announcement.js
@@ -4,8 +4,8 @@ export default function Announcement() {
   return (
     <div className="bg-maize text-darkblue py-2 text-center w-full">
       <p className="text-lg font-bold">
-        Tryouts Starting Soon! Check Back For Updates!
-        {/* Tryouts are here! <Link className="underline" href="/players/portal">Sign up now!</Link> */}
+        {/* Tryouts Starting Soon! Check Back For Updates! */}
+        Tryouts are here! <Link className="underline" href="/players/portal">Sign up in your player portal now!</Link>
       </p>
     </div>
   );
